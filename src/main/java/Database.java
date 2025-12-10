25e2f37a95325599e6a70615681f2784088a0fdd
tsakalousem <t8240237@aueb.gr>
2025-12-10 18:15:23 +0200

    Αλλαγές στην κλάση database για να δημιουργεί το αρχείο db

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,12 +4,11 @@ import java.sql.SQLException;
 
 public class Database {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/STATEBUDGET?serverTimezone=UTC";
-//Σύνδεση της java με mySQL
+    // Δημιουργεί τo SQLite database file 
+    private static final String URL = "jdbc:sqlite:statebudget.db";
+
     public static Connection getConnection() throws SQLException {
-        String user = "root";
-        String pass = " ";  
-        //Τα user και pass δεν αφορούν τον χρήστη της εφαρμογής είναι του mySQL server
-        return DriverManager.getConnection(URL, user, pass);
+        return DriverManager.getConnection(URL);
     }
 }
+
