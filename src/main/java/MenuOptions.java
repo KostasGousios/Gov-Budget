ffc23ccc36bf7d754c34f6630dd147cb0a459de7
goulatheodora <t8240248@aueb.gr>
2025-12-12 16:55:25 +0200

    διόρθωσα τα ορθογραφικά και πρόσθεσα τόνους

--- src/main/java/MenuOptions.java
+++ src/main/java/MenuOptions.java
@@ -1,10 +1,9 @@
-
 public enum MenuOptions {
      PROTHIPOURGOS_ARXH("Δημιουργία πρώτου υποθετικού προϋπολογισμού"),
      YPOYRGEIO_PAIDEIAS("Ζητάει το υπουργείο παιδείας χρήματα"),
      YPOYRGEIO_YGEIAS("Ζητάει το υπουργείο υγείας χρήματα"),
      PROTHIPOYRGOS_MESH("Πρωθυπουργός εισάγει τι ποσά  πραγματοποιήθηκαν"),
-     PROTHIPOURGOS_TELOS("Πρωθυπουργός θέλει παραδώσει τελικό προϋπολογισμό");
+     PROTHIPOURGOS_TELOS("Πρωθυπουργός θέλει να παραδώσει τελικό προϋπολογισμό");
 
      private String description;
 
